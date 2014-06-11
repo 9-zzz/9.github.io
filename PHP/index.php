1ce725e3e22a6a1f76f4c21f16a43463f7d514b3
Shahan Akhter <shahan3.14@gmail.com>
2014-06-11 17:07:14 -0400

    added a PHP dir

---
+++ PHP/index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+<?php
+// Write your if/elseif/else statement here!
+$var = 10;
+if ($var == 9){
+  echo "The condition is true";
+}else{
+  echo "The condition is false";
+}elseif($var){
+  this is a statement
+}
+?>
+    </p>
+  </body>
+</html>
